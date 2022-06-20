c7146221b08a3366517aeab43562b0cbcab85979
yshen <287049745@qq.com>
2022-06-20 15:17:06 +0800

    Deployed 70d4a6b with MkDocs version: 1.3.0

--- python/test.py
+++
@@ -1,6 +0,0 @@
-from operator import itemgetter
-
-rows = [{'name': 'Yang', 'uid': 1001}, {'name': 'James', 'uid': 1003}]
-
-rows_by_name = sorted(rows, key=itemgetter('name'))
-rows_by_uid = sorted(rows, key=itemgetter('uid'))
